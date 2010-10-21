d2f74660b8ac0ecc5eef1b65a4fd2d24c419cb2f
David Caldwell <david@porkrind.org>
2010-10-21 04:25:37 -0700

    uniq.h: functional style wrapper for stl unique()

---
+++ uniq.h
@@ -0,0 +1,28 @@
+//  Copyright (c) 2010 David Caldwell,  All Rights Reserved. -*- c++ -*-
+#ifndef __UNIQ_H__
+#define __UNIQ_H__
+
+#include <vector>
+#include <algorithm>
+
+template<class T>
+std::vector<T> uniq(std::vector<T> list)
+{
+    sort(list.begin(), list.end());
+    typename std::vector<T>::const_iterator end = unique(list.begin(), list.end());
+    list.resize(end - list.begin());
+    return list;
+}
+
+template<class T>
+std::vector<T> uniq(std::vector<T> list, bool (*binary_predicate)(typename std::vector<T>::forward_iterator a,
+                                                                  typename std::vector<T>::forward_iterator b))
+{
+    sort(list.begin(), list.end());
+    typename std::vector<T>::const_iterator end = unique(list.begin(), list.end(), binary_predicate);
+    list.resize(end - list.begin());
+    return list;
+}
+
+#endif /* __UNIQ_H__ */
+
