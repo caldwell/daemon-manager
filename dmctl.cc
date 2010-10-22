82ba753812d66a791a77e0b030ac8b128f60d06a
David Caldwell <david@porkrind.org>
2010-10-22 07:17:28 -0700

    [dmctl.cc] Add pod documentation for man page.

--- dmctl.cc
+++ dmctl.cc
@@ -68,3 +68,110 @@ int main(int argc, char **argv)
     exit(1);
 }
 
+/*
+
+=head1 NAME
+
+dmctl - Daemon Manager control
+
+=head1 SYNOPSIS
+
+  dmctl list|status|rescan
+  dmctl start|stop|restart <daemon-id>
+
+=head1 DESCRIPTION
+
+B<dmctl> allows the user to communicate and interact with the
+L<daemon-manager(1)> daemon.
+
+=head1 COMMANDS
+
+=over
+
+=item I<list>
+
+This command will list all the daemon ids that the user is allowed to control.
+
+=item I<status>
+
+This command will print a human readable list of the daemons-ids and their
+current stats.
+
+The following pieces of data are shown per daemon:
+
+=over
+
+=item I<daemon-id>
+
+The daemon id.
+
+=item I<state>
+
+One of C<stopped>, C<running>, C<stopping>, C<coolingdown>.
+
+The cooling down state happens when the daemon starts respawning too quickly. In
+order to prevent too much resource utilization, daemon-manager(1) will require
+the daemon to cool off for some period of time before respawning it.
+
+=item I<pid>
+
+The process id of the running daemon, or zero if it is not running.
+
+=item I<respawns>
+
+The number of times the daemon has been respawned by daemon-manager(1) due to it
+quitting unexpectadly.
+
+=item I<cooldown>
+
+The number of seconds left in the cooldown period.
+
+=item I<uptime>
+
+The number of seconds the daemon has been running for since the last respawn.
+
+=item I<total>
+
+The total number of seconds the daemon has been running for since the last
+start.
+
+=back
+
+=item I<rescan>
+
+Upon receiving this command, L<daemon-manager(1)> will look through the user's
+home directory (and the home directories of the other users managed by the user)
+for new daemon config files. Any new files will be loaded (and started if their
+I<autostart> option is set).
+
+It is not necessary to issue the I<rescan> command if a config file has been
+edited or deleted. I<start> and I<stop> will catch those 2 cases respectively.
+
+=item I<start> <daemon-id>
+
+This will start the daemon identified by I<< <daemon-id> >> if it hasn't already
+been started.
+
+If the daemon is cooling down then I<start> will attempt to start it
+immediately. This is useful if you are debugging a daemon that is not launching
+properly and don't want to wait for the cooldown period.
+
+=item I<stop> <daemon-id>
+
+This will stop the daemon identified by I<< <daemon-id> >> if it isn't already
+stopped.
+
+=item I<restart> <daemon-id>
+
+Currently this is just a short hand for a I<stop> command followed by a I<start>
+command.
+
+=back
+
+=head1 SEE ALSO
+
+L<daemon-manager(1)>, L<daemon.conf(5)>
+
+=cut
+
+*/
