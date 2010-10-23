b618993d62a77878eaeb2065af8db0d2a8faf6f0
David Caldwell <david@porkrind.org>
2010-10-23 16:17:02 -0700

    Add options.cc/options.h: a command line options parser

---
+++ options.cc
@@ -0,0 +1,95 @@
+//  Copyright (c) 2010 David Caldwell,  All Rights Reserved.
+
+#include "options.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class option_match {
+    public: string &lopt, &sopt;
+            option_match(string &lopt, string &sopt) : lopt(lopt), sopt(sopt) {}
+            bool operator()(string s) {
+                return s == "--"+lopt ||
+                       s.find("--"+lopt+"=") == 0 ||
+                       sopt.size() && s == "-" + sopt;
+            }
+};
+
+options::options(int c, char **v)
+{
+    end = -1;
+    for (int i=1; i<c; i++)
+        if (string(v[i]) == "--" && end == (size_t) -1)
+            end = i-1;
+        else
+            args.push_back(v[i]);
+    if (end == (size_t) -1)
+        end = args.size();
+}
+
+bool options::get(string long_opt, char short_opt_char, arg_type type)
+{
+    string short_opt; short_opt += short_opt_char;
+    arg="";
+    argm.clear();
+    vector<string>::iterator o = args.begin();
+    while(1) {
+        o = find_if(o, args.begin()+end, option_match(long_opt, short_opt));
+        if (o == args.begin()+end)
+            break;
+
+        string opt = *o;
+        o = args.erase(o);
+        end--;
+        bool islong = opt.find("--") == 0;
+        string val;
+        if (type == arg_none) {
+            if (opt.find("=") != opt.npos) {
+                bad.push_back("\""+opt+"\" does not take an argument");
+                continue;
+            }
+            val = opt;
+        }
+        if (type == arg_required) {
+            if (islong) {
+                size_t vo = opt.find('=');
+                if (vo == opt.npos) {
+                    bad.push_back("missing argument for "+opt);
+                    continue;
+                }
+                val = opt.substr(vo+1);
+            } else {
+                if (o == args.begin()+end) {
+                    bad.push_back("missing argument for -"+short_opt);
+                    continue;
+                }
+                val = *o;
+                o = args.erase(o);
+                end--;
+            }
+        }
+        argm.push_back(val);
+        arg = val;
+    }
+    return argm.size();
+}
+
+bool options::get(string long_opt, arg_type type)
+{
+    return get(long_opt, 0, type);
+}
+
+bool options::bad_args()
+{
+    bool is_bad = false;
+    for (vector<string>::iterator ba = bad.begin(); ba != bad.end(); ba++)
+        fprintf(stderr, "%s\n", ba->c_str());
+
+    for (vector<string>::iterator a = args.begin(); a != args.begin()+end; a++)
+        if (a->size() && a->at(0) == '-') {
+            fprintf(stderr, "Unkown argument \"%s\"\n", a->c_str());
+            is_bad = true;
+        }
+    return bad.size() || is_bad;
+}
---
+++ options.h
@@ -0,0 +1,48 @@
+//  Copyright (c) 2010 David Caldwell,  All Rights Reserved. -*- c++ -*-
+#ifndef __OPTIONS_H__
+#define __OPTIONS_H__
+
+#include <string>
+#include <vector>
+
+enum arg_type { arg_none, arg_required, /* arg_optional not supported yet */ };
+
+class options {
+  public:
+    options(int c, char **v);
+    bool get(std::string long_opt, char short_opt, arg_type type = arg_none);
+    bool get(std::string long_opt, arg_type type = arg_none);
+    std::vector<std::string> argm; // after get this is set to an array of all the options of that type
+    std::string arg;               // after get this is set to the last option of that type or "" if nothing exists.
+    bool bad_args();               // prints a list of errors to stderr.
+    std::vector<std::string> args; // Arguments that haven't been parsed yet.
+  private:
+    std::vector<std::string> bad;
+    size_t end;
+};
+
+// The standard technique:
+// int main(int argv, char **argv)
+// {
+//    options o(argc, argv);
+//    if (o.get("help",       'h'))               { /* */ }
+//    if (o.get("config",     'c', arg_required)) { /* o.arg is set to last -c or --config on the command line */}
+//    if (o.get("verbose",    'v'))               { /* o.argm.size() is a count of how many were specified (-v -v -v) */ }
+//    if (o.get("output",     'o', arg_required)) { /* o.argm is vactor<string> of --output or -o command line args:
+//                                                     "-o f1 -o f2 --output f3" would give { "f1", "f2", "f3" } */ }
+//    if (o.get("foreground"))                    { /* Short option is not required */ }
+//    if (o.get("version"))                       { /* o.arg == "--version" for flags */ }
+//    if (o.bad_args() ||                         // o.bad_args() will print to stderr and return true if there were errors [1].
+//        o.args.size() != 1)                     // Anything not parsed is left over in o.args.
+//           usage(argv[0], EXIT_FAILURE);        // argv is left untouched.
+//    ...
+//
+// [1] Missing argument errors are caught and saved during calls to o.get(). o.bad_args() will also go through
+// the remaining arguments (o.args) and report unknown argument errors if something in there starts with '-'.
+//
+// The '--' arg separator is supported. The first one encountered will set it up so that o.get() and
+// o.bad_args() will never go past that point when parsing options. Everything after it will still be
+// available in o.args(). The '--' itself will be removed from the list though.
+
+#endif /* __OPTIONS_H__ */
+
