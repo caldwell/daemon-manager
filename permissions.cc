4145854c60289aed0e1a649eec48c91d05bc8d0d
David Caldwell <david@porkrind.org>
2010-10-22 17:57:18 -0700

    [permissions.cc] Use throw_str() and throw_strerr() to unify exception thowing.

--- permissions.cc
+++ permissions.cc
@@ -12,10 +12,10 @@ struct stat permissions::check(string file, int bad_modes, uid_t required_uid, g
 {
     const char *path = file.c_str();
     struct stat st;
-    if (stat(path, &st)) throw strprintf("%s doesn't exist", path);
-    if (st.st_mode & bad_modes & 0002)   throw strprintf("%s can't be world writable", path);
-    if (st.st_mode & bad_modes & 0111)   throw strprintf("%s can't be executable", path);
-    if (required_uid != (uid_t)-1 && st.st_uid != required_uid) throw strprintf("%s must be owned by \"%s\"", path, name_from_uid(required_uid).c_str());
-    if (required_gid != (gid_t)-1 && st.st_gid != required_gid) throw strprintf("%s must have group \"%s\"",path, name_from_gid(required_gid).c_str());
+    stat(path, &st) == 0 || throw_str("%s doesn't exist", path);
+    if (st.st_mode & bad_modes & 0002)   throw_str("%s can't be world writable", path);
+    if (st.st_mode & bad_modes & 0111)   throw_str("%s can't be executable", path);
+    if (required_uid != (uid_t)-1 && st.st_uid != required_uid) throw_str("%s must be owned by \"%s\"", path, name_from_uid(required_uid).c_str());
+    if (required_gid != (gid_t)-1 && st.st_gid != required_gid) throw_str("%s must have group \"%s\"",path, name_from_gid(required_gid).c_str());
     return st;
 }
