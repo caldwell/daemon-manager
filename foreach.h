ce3c0fd39eb489481949685cd1492c547de2319d
David Caldwell <david@porkrind.org>
2010-10-28 07:58:43 -0700

    [foreach.h] Support "break;"

--- foreach.h
+++ foreach.h
@@ -11,8 +11,10 @@ template<class T> class bool_container {
 
 #define foreach(var, container)                                         \
     if (bool_container<typeof(container)> __c = container) {}  else     \
-        for (typeof(container.begin()) _var_it = __c.item.begin(); _var_it != __c.item.end(); _var_it++) \
-            if (bool __stop = false) {} else                            \
-                for (var = *_var_it; !__stop; __stop = true)
+    if (bool __break = false) {} else                                   \
+    for (typeof(container.begin()) _var_it = __c.item.begin(); !__break && _var_it != __c.item.end(); _var_it++) \
+        if (bool __once = false) {} else                                \
+        if (!(__break = true)) {} else                                  \
+        for (var = *_var_it; !__once; __once = true, __break=false)
 
 #endif /* __FOREACH_H__ */
