fa7edd0ce5466bf23b5aa31857036671cc8bf77d
David Caldwell <david@porkrind.org>
2011-02-06 22:38:43 -0800

    [foreach.h] Fix foreach crashing on foreach(x, f()).
    The const reference to "container" needs to be in a local variable and not
    hidden in a class. This makes c++ extend the life of "container" if it is
    temporary (ie, the return value of a function) to the life of the local
    const ref.

--- foreach.h
+++ foreach.h
@@ -17,11 +17,12 @@ template<class T> T *__begin(T x[])           { return &x[0]; }
 template<class T> T *__end(T x[], size_t len) { return &x[len/sizeof(*x)]; }
 
 #define foreach(var, container)                                         \
-    if (bool_container<const typeof(container)&> __c = container) {}  else    \
-    if (bool_container<typeof(__begin(__c.item))> __b = __begin(__c.item)) {}  else \
-    if (bool_container<typeof(__end(__c.item,0))> __e = __end(__c.item, sizeof(__c.item)))   {}  else \
+    if (bool __once_1 = true)                                           \
+    for (const typeof(container)& __c = (container); __once_1; __once_1 = false) \
+    if (bool_container<typeof(__begin(__c))> __b = __begin(__c)) {}  else \
+    if (bool_container<typeof(__end(__c,0))> __e = __end(__c, sizeof(__c)))   {}  else \
     if (bool __break = false) {} else                                   \
-    for (typeof(__begin(__c.item)) _var_it = __b.item; !__break && _var_it != __e.item; _var_it++) \
+    for (typeof(__begin(__c)) _var_it = __b.item; !__break && _var_it != __e.item; _var_it++) \
         if (bool __once = false) {} else                                \
         if (!(__break = true)) {} else                                  \
         for (var = *_var_it; !__once; __once = true, __break=false)
