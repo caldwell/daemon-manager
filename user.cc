08a15ff744117d682bc50baf4afc1433d624222e
David Caldwell <david@porkrind.org>
2011-11-13 14:54:47 -0800

    [user.cc] Don't let command_socket be uninitialized.
    Re: a83e1c33e1

--- user.cc
+++ user.cc
@@ -23,6 +23,7 @@ using namespace std;
 map<string,user*> users;
 
 user::user(string name)
+        : command_socket(-1)
 {
     struct passwd *p = getpwnam(name.c_str());
     if (!p) throw_str("No user named \"%s\"", name.c_str());
@@ -30,6 +31,7 @@ user::user(string name)
 }
 
 user::user(uid_t uid)
+        : command_socket(-1)
 {
     struct passwd *p = getpwuid(uid);
     if (!p) throw_str("No user with uid %d", uid);
