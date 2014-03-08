6ad7b7c06d1f76e1e2038906c450796fe502bd90
David Caldwell <david@porkrind.org>
2014-03-08 11:36:27 -0800

    [daemon-manager-completion.bash] POSIX awk has no -e option.

--- contrib/daemon-manager-completion.bash
+++ contrib/daemon-manager-completion.bash
@@ -8,10 +8,10 @@ _dmctl()
         local IFS=$',' # "dmctl list" outputs comma separated list of daemons
         # Remove user's own prefix from the daemon name. But leave it in for other user's they may control
         local all=( $(dmctl list | sed -e "s,$USER/,,g") \
-                    $(dmctl --help | awk -e '/dmctl/ { print gensub("\\|",",","g",$3 ? "" : $2) }') )
+                    $(dmctl --help | awk '/dmctl/ { print gensub("\\|",",","g",$3 ? "" : $2) }') )
     else
         # Only include commands that come after the daemon id (opposite of the above which only takes commands with no id).
-        local all=( $(dmctl --help | awk -e '/dmctl/ { print gensub("\\|"," ","g",$3 ? $3 : "") }') )
+        local all=( $(dmctl --help | awk '/dmctl/ { print gensub("\\|"," ","g",$3 ? $3 : "") }') )
     fi
 
     # Now prune out the completions that don't match
