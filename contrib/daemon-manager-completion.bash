2da7714d8dd3b6ee4a35ef621f5386fbd4dcf549
David Caldwell <david@porkrind.org>
2014-03-08 20:00:40 -0800

    [daemon-manager-completion.bash] Use only POSIX awk functions. Sigh.
    Debian/Ubuntu only have "mawk" installed by default, which doesn't have gensub().

--- contrib/daemon-manager-completion.bash
+++ contrib/daemon-manager-completion.bash
@@ -4,15 +4,17 @@ _dmctl()
 {
     COMPREPLY=()
 
+    local awk_func='function pipe_to_comma(s) { gsub("\\|", ",", s); return s }'
+
     if (($COMP_CWORD == 1)); then
         local IFS=$',' # "dmctl list" outputs comma separated list of daemons
         # Include both the prefixed and non prefixed version, since dmctl works with either.
         # Also include the commands that take no id (status, rescan)
         local all=( $(dmctl list | sed -e "s:\([^,/]*/\)\([^,]*\):\1\2,\2:g") \
-                    $(dmctl --help | awk '/dmctl/ { print gensub("\\|",",","g",$3 ? "" : $2) }') )
+                    $(dmctl --help | awk "$awk_func;"'/dmctl/ { print pipe_to_comma($3 ? "" : $2) }') )
     else
         # Only include commands that come after the daemon id (opposite of the above which only takes commands with no id).
-        local all=( $(dmctl --help | awk '/dmctl/ { print gensub("\\|"," ","g",$3 ? $3 : "") }') )
+        local all=( $(dmctl --help | awk "$awk_func;"'/dmctl/ { print pipe_to_comma($3 ? $3 : "") }') )
     fi
 
     # Now prune out the completions that don't match
