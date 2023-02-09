990d67469ffd8c5048a3043d312ea3551a7b24d9
David Caldwell <david@porkrind.org>
2023-02-09 14:08:04 -0800

    daemon-manager.cc: Add `configfile` command socket command to return its path

--- daemon-manager.cc
+++ daemon-manager.cc
@@ -627,7 +627,7 @@ static string do_command(string command_line, user *user, vector<class daemon*> 
     string arg = space != command_line.npos ? command_line.substr(space+1, command_line.length()) : "";
     log(LOG_DEBUG, "line: \"%s\" cmd: \"%s\", arg: \"%s\"\n", command_line.c_str(), cmd.c_str(), arg.c_str());
 
-    const string valid_commands[] = { "list", "status", "rescan", "start", "stop", "restart", "logfile", "pid" };
+    const string valid_commands[] = { "list", "status", "rescan", "start", "stop", "restart", "logfile", "configfile", "pid" };
 
     if (find(valid_commands, valid_commands + lengthof(valid_commands), cmd) == valid_commands + lengthof(valid_commands) && cmd.compare(0,5,"kill-") != 0)
         throw_str("bad command \"%s\"", cmd.c_str());
@@ -673,6 +673,7 @@ static string do_command(string command_line, user *user, vector<class daemon*> 
     else if (cmd == "stop")    daemon->stop();
     else if (cmd == "restart") { daemon->stop(); daemon->start(); }
     else if (cmd == "logfile") return "OK: " + daemon->log_file();
+    else if (cmd == "configfile") return "OK: " + daemon->config_file;
     else if (cmd == "pid")     if (!daemon->current.pid) throw_str("\"%s\" isn't running", daemon->id.c_str());
                                else return strprintf("OK: %d\n", daemon->current.pid);
     else if (cmd.compare(0,5,"kill-") == 0) {
